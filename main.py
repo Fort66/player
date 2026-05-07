e75a16ef4bfad0f979b76b904bf0948d96e53a70
Fort66 <mamonov.1966@yandex.ru>
2026-05-07 15:39:21 +0300

    final commit

--- main.py
+++
@@ -1,6 +0,0 @@
-from functions.function_main import main
-
-
-if __name__== '__main__':
-    main()
-
--- screens/py_screens/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Добавляем путь этой папки в начало списка поиска модулей
-path = os.path.dirname(os.path.abspath(__file__))
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-from . import player_rc
